9da6a866fd74eb8643f2421b221f23f63114262f
Mohamed Gamal <mohamadgamal2021@gmail.com>
2023-09-26 19:12:14 +0200

    message

--- Debug/CAN.h
+++
@@ -1,2 +0,0 @@
-const uint8_t CAN_image_0[4832];
-
--- Debug/TX/TX.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-TX/TX.obj: ../TX/TX.c
-TX/TX.obj: E:/EUI\ workspace/workspace_new/CAN/def.h
-
-../TX/TX.c:
-
-E:/EUI\ workspace/workspace_new/CAN/def.h:
-
